f6f313771c3c40200e3656647fdedc8fc161b4ff
seraph <quietsolitvde@gmail.com>
2026-05-31 13:24:05 +0700

    Simplify first pseudo code section

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-// Write a constructor for making "Book" objects
-    //The constructor should contain the book's title, author, number of pages, read status, 
-    // an info() method that reports the object's information like so: 
-        // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
-
-// Create a library variable that holds books
 // Write a constructor for making "book" objects
+// Create a library variable that holds books
 // Write a function that create a book and add it to the library variable
 
 // Write a function that loops through the library to display books on a table in html
@@ -31,3 +26,11 @@ function addBookToLibrary(title, author, numOfPages, readStatus) {
     const book = new Book(title, author, numOfPages, readStatus);
     library.push(book);
 }
+
+addBookToLibrary("Cho Tôi Xin Một Vé Đi Tuổi Thơ", "Nguyễn Nhật Ánh", 208, true);
+addBookToLibrary("Mắt Biếc", "Nguyễn Nhật Ánh", 320, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
+addBookToLibrary("Atomic Habits", "James Clear", 320, true);
+addBookToLibrary("Dune", "Frank Herbert", 688, false);
+
+console.log(library);
